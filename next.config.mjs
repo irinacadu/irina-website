9fb6cd86d3e98b67be2ce1f9372b802b4c8f8391
Irina C. <irinacadu@gmail.com>
2024-08-27 23:16:35 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
+
 const nextConfig = {};
 
 export default nextConfig;
+module.exports = {
+    i18n,
+    // Otras configuraciones de Next.js
+  };
